f21f045a1afa8ead9f917892559a1a6337fb4420
ceciliachan1979 <75919064+ceciliachan1979@users.noreply.github.com>
2025-03-11 15:09:09 -0700

    Hide implementation details

--- inc/ekans-internals.h
+++ inc/ekans-internals.h
@@ -2,4 +2,16 @@
 
 #include <ekans.h>
 
+typedef enum { number, boolean } ekan_type;
+
+typedef struct ekans_value {
+  ekan_type type;
+  union {
+    int n;
+    bool b;
+  } value;
+  struct ekans_value *prev;
+  struct ekans_value *next;
+} ekans_value;
+
 void append(ekans_value *new_value);
--- inc/ekans.h
+++ inc/ekans.h
@@ -4,17 +4,7 @@
 
 typedef enum { false, true } bool;
 
-typedef enum { number, boolean } ekan_type;
-
-typedef struct ekans_value {
-  ekan_type type;
-  union {
-    int n;
-    bool b;
-  } value;
-  struct ekans_value *prev;
-  struct ekans_value *next;
-} ekans_value;
+typedef struct ekans_value ekans_value;
 
 ekans_value *create_number_value(int v);
 
