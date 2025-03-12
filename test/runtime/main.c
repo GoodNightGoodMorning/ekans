0c5995b5824a64f5fb6b377e20ebce7ed1b697b7
gapry <gapry@protonmail.com>
2025-03-12 23:15:07 +0800

    call finalize_ekans after each test case to avoid memory leak

--- test/runtime/main.c
+++ test/runtime/main.c
@@ -5,30 +5,39 @@
 
 void test_initialize_ekans() {
   initialize_ekans();
-  assert(head.next == &tail);
-  assert(tail.prev == &head);
+  {
+    assert(head.next == &tail);
+    assert(tail.prev == &head);
+  }
+  finalize_ekans();
   printf("[%s] passed\n", __FUNCTION__);
 }
 
 void test_create_number_value() {
   initialize_ekans();
-  ekans_value* const v = create_number_value(20250312);
-  assert(v->type == number);
-  assert(v->value.n == 20250312);
-  assert(head.next == v);
-  assert(v->prev == &head);
-  assert(v->next == &tail);
+  {
+    ekans_value* const v = create_number_value(20250312);
+    assert(v->type == number);
+    assert(v->value.n == 20250312);
+    assert(head.next == v);
+    assert(v->prev == &head);
+    assert(v->next == &tail);
+  }
+  finalize_ekans();
   printf("[%s] passed\n", __FUNCTION__);
 }
 
 void test_create_boolean_value() {
   initialize_ekans();
-  ekans_value* const v = create_boolean_value(true);
-  assert(v->type == boolean);
-  assert(v->value.b == true);
-  assert(head.next == v);
-  assert(v->prev == &head);
-  assert(v->next == &tail);
+  {
+    ekans_value* const v = create_boolean_value(true);
+    assert(v->type == boolean);
+    assert(v->value.b == true);
+    assert(head.next == v);
+    assert(v->prev == &head);
+    assert(v->next == &tail);
+  }
+  finalize_ekans();
   printf("[%s] passed\n", __FUNCTION__);
 }
 
