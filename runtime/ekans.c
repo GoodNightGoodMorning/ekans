86c1c1275f66691a2d7f7d88550f42b4f8b2eb81
gapry <gapry@protonmail.com>
2025-03-12 23:30:21 +0800

    design-fix: rename the macro

--- runtime/ekans.c
+++ runtime/ekans.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MARK_BITS 65536
+#define EKANS_MARK_BITS 65536
 
 stack_slot* g_stack_slots = NULL;
 
@@ -138,13 +138,13 @@ void mark_recursively(ekans_value* obj) {
 }
 
 void mark_this(ekans_value* obj) {
-  obj->type |= MARK_BITS;
+  obj->type |= EKANS_MARK_BITS;
 }
 
 void reset_this(ekans_value* obj) {
-  obj->type &= ~MARK_BITS;
+  obj->type &= ~EKANS_MARK_BITS;
 }
 
 bool marked(ekans_value* obj) {
-  return (obj->type & MARK_BITS) != 0;
+  return (obj->type & EKANS_MARK_BITS) != 0;
 }
